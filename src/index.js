fc737cc15865a457f3c8b6108095184329fab086
daky-2798 <dakshtandel.27@gmail.com>
2021-06-26 12:53:01 +0530

    Added middleware and redux devtols

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore,combineReducers} from 'redux';
+import {createStore,combineReducers,applyMiddleware,compose} from 'redux';
 import counterReducer from './store/counter';
 import resultReducer from './store/result';
 import { Provider } from 'react-redux';
@@ -11,9 +11,23 @@ import { Provider } from 'react-redux';
 const rootReducer = combineReducers({
     ctr : counterReducer,
     res : resultReducer
-})
+});
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[MiddleWare] Dispatching',action);
+            const result = next(action);
+            console.log('[MiddleWare] next state',store.getState());
+            return result;
+        }
+
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
